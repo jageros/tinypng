38d2ba5c52ef96b7b0ff8110f5de9fb1cd2eef96
Jay Leung <lhj168os@gmail.com>
2020-04-10 17:05:05 +0800

    create pro

---
+++ go.mod
@@ -0,0 +1,5 @@
+module cimage
+
+go 1.12
+
+require github.com/gwpp/tinify-go v0.0.0-20170613055357-77b9df15f343
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gwpp/tinify-go/tinify"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	Tinify.SetKey("pJ3FQdt95CnPT0xc5TDkj52m45Kzn8Tt")
+	compDir("./imgs/", "./cpimgs/")
+}
+
+func compDir(inDir, outDir string) {
+	files, err := ioutil.ReadDir(inDir)
+	if err != nil {
+		log.Panicf("ReadDir error: %v", err)
+	}
+	if len(files) <= 0 {
+		log.Panicf("Dir %s not has file", inDir)
+	}
+	if !strings.HasSuffix(inDir, "/") {
+		inDir = inDir + "/"
+	}
+	if !strings.HasSuffix(outDir, "/") {
+		outDir = outDir + "/"
+	}
+	for _, f := range files {
+		fileName := f.Name()
+		if strings.HasSuffix(fileName, ".jpg") || strings.HasSuffix(fileName, ".jpeg") || strings.HasSuffix(fileName, ".png") {
+			compImage(inDir, outDir, fileName)
+		}
+	}
+}
+
+func compImage(inPath, outPath, fileName string) {
+	start := time.Now()
+	iPath := fmt.Sprintf("%s%s", inPath, fileName)
+	log.Printf("Start Compress: %s ...", iPath)
+	source, err := Tinify.FromFile(iPath)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	oPath := fmt.Sprintf("%s%s", outPath, fileName)
+	err = source.ToFile(oPath)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	err = os.Remove(iPath)
+	if err != nil {
+		log.Printf("Delete file err: %v", err)
+	}
+	takeTime := time.Now().Sub(start).Seconds()
+	log.Printf("Compress successful: %s (takes %fs)", oPath, takeTime)
+}
